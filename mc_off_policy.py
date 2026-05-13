90cc014f4ccc807d7da9debb760a55712da1d910
Lucas Chen <lucascehn@gmail.com>
2026-05-12 23:50:07 -0700

    fix mc off policy bugs

--- mc_off_policy.py
+++ mc_off_policy.py
@@ -6,9 +6,13 @@ S = [-1, -1, 2, 5, 100]
 actions = [-1, 1]
 # calculated value for each state
 Vs = [[] for _ in range(len(S))]
+# calculated weight for each state
+Ws = [[] for _ in range(len(S))]
 # calculated value for each state-action pair
 Qs = dict()
 Qs_avg = dict()
+# calculated weight for each state-action pair
+QWs = dict()
 # discount rate
 gamma = 0.9
 # number of episodes to run
@@ -37,7 +41,7 @@ def get_importance_sampling_weight(a, s, Qs_avg):
     # off policy
     pi = 1 if a == 1 else 0
     greedy_a = get_greedy_action(Qs_avg, s)
-    b = epsilon / len(actions) if a == greedy_a else 1 - epsilon + epsilon / len(actions)
+    b = 1 - epsilon + epsilon / len(actions) if a == greedy_a else epsilon / len(actions)
     return pi / b
 
 for _ in range(5):
@@ -57,12 +61,17 @@ for _ in range(5):
                 # don't wanna get stuck in state 0, always go right
                 a = 1
             else:
-                a = random.choice(actions) if p < (epsilon / len(actions)) else get_greedy_action(Qs, s)
+                # get_greedy_action may return left when empty so we need to check if Qs_avg is empty
+                a = random.choice(actions) if p < (epsilon / len(actions)) or len(Qs) == 0 else get_greedy_action(Qs_avg, s)
             next_s = get_next_state(s, a)
             history.append((s, a, S[next_s]))
             s = next_s
 
         G = 0
+        # W = ∏ π(aₜ|sₜ) / b(aₜ|sₜ) for t=0 to T-1 for a single episode
+        W = 1.0
+        # first time weight
+        ft_w = dict()
         for h in reversed(history):
             s, a, r = h
             # calculate return G for the first time we visit state s
@@ -70,19 +79,31 @@ for _ in range(5):
             # this is the first time we visit state s
             ft[s] = G
             ft_q[(s, a)] = G
+            ft_w[s] = W
+            if a != 1:
+                break
+            W *= get_importance_sampling_weight(a, s, Qs_avg)
 
         for s in range(len(S)):
             if ft[s] is not None:
                 Vs[s].append(ft[s])
+                Ws[s].append(ft_w[s])
         for (s, a), v in ft_q.items():
             Qs[(s, a)] = Qs.get((s, a), []) + [v]
+            QWs[(s, a)] = QWs.get((s, a), []) + [ft_w[s]]
 
-    Vs_avg = [sum(vs) / len(vs) if vs else 0 for vs in Vs]
-    # this can be normalized with sum of importance sampling weights if we want to get the actual expected value
-    # however i am lazy
-    Qs_avg = {
-        (s, a): (sum(vs) / len(vs)) * get_importance_sampling_weight(a, s, Qs_avg) for (s, a), vs in Qs.items()
-    }
+    Vs_avg = []
+    for vs, ws in zip(Vs, Ws):
+        gg = 0
+        for i in range(len(vs)):
+            gg += vs[i] * ws[i]
+        Vs_avg.append(gg / sum(ws) if sum(ws) > 0 else 0)
+
+    for (s, a), vs in Qs.items():
+        gg = 0
+        for i in range(len(vs)):
+            gg += vs[i] * QWs[(s, a)][i]
+        Qs_avg[(s, a)] = gg / sum(QWs[(s, a)]) if sum(QWs[(s, a)]) > 0 else 0
 
     print("Estimated value for each state:", Vs_avg)
     print("Estimated value for each state-action pair:", Qs_avg)
@@ -96,8 +117,8 @@ for _ in range(5):
     print("\n" + "="*50 + "\n")
 
 """
-Estimated value for each state: [24.885022833181814, 28.761136481313127, 30.067929423700008, 29.558582758290964, 0]
-Estimated value for each state-action pair: {(3, 1): 38.75171462944419, (2, 1): 32.41305075124295, (1, 1): 29.806293827005142, (0, 1): 25.52310034172494, (1, -1): -0.0, (2, -1): 0.0, (3, -1): 0.0}
+Estimated value for each state: [77.75, 68.97628636900217, 95.0, 100.0, 0]
+Estimated value for each state-action pair: {(3, 1): 100.0, (2, 1): 95.0, (1, 1): 87.5, (0, 1): 77.75, (1, -1): 68.97500000000001}
 
 Optimal policy:
 At state 0, take action 1
@@ -107,8 +128,8 @@ At state 3, take action 1
 
 ==================================================
 
-Estimated value for each state: [27.184932934158923, 31.316592149065475, 32.74065794341546, 31.675617191995986, 0]
-Estimated value for each state-action pair: {(3, 1): 1710.2931834121691, (2, 1): 1340.3222189118555, (1, 1): 1258.6636859629566, (0, 1): 1087.397317366357, (1, -1): -0.0, (2, -1): 0.0, (3, -1): 0.0}
+Estimated value for each state: [77.74954822689472, 68.97628240367989, 94.11188715848283, 81.31483001746896, 0]
+Estimated value for each state-action pair: {(3, 1): 81.47833744264486, (2, 1): 94.11188715848283, (1, 1): 87.49998640566616, (0, 1): 77.74954822689472, (1, -1): 68.97500000000001, (3, -1): 45.88179297500002}
 
 Optimal policy:
 At state 0, take action 1
@@ -118,8 +139,8 @@ At state 3, take action 1
 
 ==================================================
 
-Estimated value for each state: [28.02335117086263, 32.24816796762514, 33.72018663070086, 32.47976109291275, 0]
-Estimated value for each state-action pair: {(3, 1): 1678.6755942090488, (2, 1): 1369.271399344859, (1, 1): 1293.926718705231, (0, 1): 1120.9340468345051, (1, -1): 0.0, (2, -1): 0.0, (3, -1): 0.0}
+Estimated value for each state: [77.74911704298931, 68.97627892567625, 93.3709389075485, 77.01471086718648, 0]
+Estimated value for each state-action pair: {(3, 1): 77.1222317974499, (2, 1): 93.3709389075485, (1, 1): 87.49997343039388, (0, 1): 77.74911704298931, (1, -1): 68.97500000000001, (3, -1): 45.88179297500002}
 
 Optimal policy:
 At state 0, take action 1
@@ -129,8 +150,8 @@ At state 3, take action 1
 
 ==================================================
 
-Estimated value for each state: [28.208097973517244, 32.453442192796935, 34.15382126767127, 32.819466702624155, 0]
-Estimated value for each state-action pair: {(3, 1): 1688.1106134049592, (2, 1): 1381.5008012944695, (1, 1): 1309.5375656361657, (0, 1): 1128.3239189406897, (1, -1): 0.0, (2, -1): 0.0, (3, -1): 0.0}
+Estimated value for each state: [77.74880538020939, 68.97627876386046, 92.80163958714262, 76.0311823424541, 0]
+Estimated value for each state-action pair: {(3, 1): 76.28688204590956, (2, 1): 92.80163958714262, (1, 1): 87.49996405131361, (0, 1): 77.74880538020939, (1, -1): 68.97500000000001, (3, -1): 53.70868895832536}
 
 Optimal policy:
 At state 0, take action 1
@@ -140,8 +161,8 @@ At state 3, take action 1
 
 ==================================================
 
-Estimated value for each state: [29.470953799276487, 33.85661533252943, 35.64956987890577, 34.39614321184238, 0]
-Estimated value for each state-action pair: {(3, 1): 1800.6906184192376, (2, 1): 1438.261155626326, (1, 1): 1363.3845156406078, (0, 1): 1178.8381519710595, (1, -1): 0.0, (2, -1): 0.0, (3, -1): 0.0}
+Estimated value for each state: [77.74798557500003, 68.97626510258175, 91.4924122341105, 69.78811791606101, 0]
+Estimated value for each state-action pair: {(3, 1): 69.9852926287545, (2, 1): 91.4924122341105, (1, 1): 87.4999393803122, (0, 1): 77.74798557500003, (1, -1): 68.97500000000001, (3, -1): 53.70868895832536}
 
 Optimal policy:
 At state 0, take action 1
